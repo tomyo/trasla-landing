1c8907ff86b3844622db435a0d1120cbb528a484
Tomas Hayes <tomas@hayes.com.ar>
2018-02-27 12:43:56 -0300

    Implementing cache update in sw

--- sw.js
+++ sw.js
@@ -1,30 +1,39 @@
 self.addEventListener('install', function(event) {
-  console.log('event', event)
-  event.waitUntil(
-    caches.open('v1').then(function(cache) {
-      return cache.addAll([
-        '/sw.js',
-        '/modules/photoTilt/photoTilt.js',
-        '/modules/photoTilt/style.css',
-        '/index.html',
-        '/index.css',
-        '/img/trasla.jpg',
-        '/fonts/koara_regular.otf',
-      ]);
-    })
-  );
+  console.log('Install Event', event);
+  event.waitUntil(precache());
 });
 
 self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request).then(function(response) {
-      console.log('fetch', event, response);
-      return response || fetch(event.request).then(function(response) {
-        return caches.open('v1').then(function(cache) {
-          cache.put(event.request, response.clone());
-          return response;
-        });
-      });
-    })
-  );
+  event.respondWith(fromCache(event.request));
+  event.waitUntil(update(event.request));
 });
+
+function precache() {
+  return caches.open('v1').then(function (cache) {
+    return cache.addAll([
+      '/sw.js',
+      '/modules/photoTilt/photoTilt.js',
+      '/modules/photoTilt/style.css',
+      '/index.html',
+      '/index.css',
+      '/img/trasla.jpg',
+      '/fonts/koara_regular.otf',
+    ]);
+  });
+}
+
+function fromCache(request) {
+  return caches.open('v1').then(function (cache) {
+    return cache.match(request).then(function (matching) {
+      return matching || Promise.reject('no-match');
+    });
+  });
+}
+
+function update(request) {
+  return caches.open('v1').then(function (cache) {
+    return fetch(request).then(function (response) {
+      return cache.put(request, response);
+    });
+  });
+}
